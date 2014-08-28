6a34234faea3340f6eece6b62f14d376a94b5f40
Jeremy Jones <theamazingjones@gmail.com>
2014-08-27 23:35:36 -0700

    added input

--- Hello.java
+++ Hello.java
@@ -1,8 +1,15 @@
+import java.util.*;
 class Hello
 {
 	public static void main(String []args)
 	{
 		System.out.println("Hello, World");
 		System.out.println("first edit using VIM!");
+		
+		Scanner in = new Scanner (System.in);
+		
+		int age = 0;
+		System.out.println("Enter your age: ");
+		age = in.nextInt();
 	}
 }
